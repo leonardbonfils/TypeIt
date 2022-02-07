aad0603e50c9f27aabf5421ddf73d5901b29ff57
L.B <6782987+EK230948230598@users.noreply.github.com>
2022-02-07 08:44:21 -0500

    Node.js initial structure with TypeScript

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+function greeter(message: string): void {
+  console.log(message);
+}
+
+greeter("Yay! This works!");
