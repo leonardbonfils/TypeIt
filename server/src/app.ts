a28ea62ae9ff2be6ee97f7b9d2bc39b083a2afcd
L.B <6782987+EK230948230598@users.noreply.github.com>
2022-02-07 12:21:35 -0500

    Initialized the Node.js back-end and Vue front-end

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,7 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World 2!");
+});
+
+app.post("/endpoint", (req, res) => {
+  res.json(req.params[0]);
+});
+
+app.get("/testGET", (req, res) => {
+  res.send("Testing new GET.");
 });
 
 app.listen(port, () => {
